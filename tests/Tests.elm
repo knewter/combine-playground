41319da3373568593e3ed312ea3851e02c8ee29f
Josh Adams <josh.rubyist@gmail.com>
2016-09-27 18:47:11 -0500

    Neat now I understand `app`

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -3,7 +3,7 @@ module Tests exposing (..)
 import Test exposing (..)
 import Expect
 import Combine.Num exposing (int)
-import Combine exposing (parse)
+import Combine exposing (parse, app)
 
 
 all : Test
@@ -12,4 +12,7 @@ all =
         [ test "parsing integers" <|
             \() ->
                 Expect.equal (parse int "123") ( Ok 123, { input = "", position = 3 } )
+        , test "parsing integers using `app`" <|
+            \() ->
+                Expect.equal (app int { input = "123", position = 0 }) ( Ok 123, { input = "", position = 3 } )
         ]
